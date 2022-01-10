a854de910fe9dff8f02df947b03ef6690e996ed5
Youssef Chouay <ychou031@uottawa.ca>
2022-01-09 22:26:34 -0500

    basic web functionalities added

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://Youssef:Stephcurry30@cluster0.tyhsd.mongodb.net/test?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
--- routes/web/home.js
+++ routes/web/home.js
@@ -1,4 +1,9 @@
 var express = require("express");
+var passport = require("passport");
+
+var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated;
+
+var User = require("../../models/user");
 
 var router = express.Router();
 
@@ -14,5 +19,52 @@ router.get("/home", function(req,res){
 router.get("/about", function(req, res){
     res.render("home/about");
 });
+
+
+router.post("/login", passport.authenticate("login", {
+    successRedirect:"/",
+    failureRedirect:"/login",
+    failureFlash:true
+}));
+
+router.get("/login", function(req,res){
+    res.render("home/login");
+});
+
+router.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/home");
+});
+
+router.get("/signup", function(req,res){
+    res.render("home/signup");
+});
+
+router.post("/signup", function(req,res,next){
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({email: email},function(err,user){
+        if(err){return next(err);}
+        if(user){
+            req.flash("error", "There's already an account with this email");
+            return res.redirect("/signup");
+        }
+
+        var newUser = new User({
+            username:username,
+            password:password,
+            email:email
+        });
+
+        newUser.save(next);
+    });
+
+}, passport.authenticate("login", {
+    successRedirect:"/",
+    failureRedirect:"/signup",
+    failureFlash:true
+}));
 
 module.exports = router;
